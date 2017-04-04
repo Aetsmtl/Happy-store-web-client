bfd58c6e8fcbab39218d0d508c2c93e29857a94e
Adneom1 <Adneom1@DELL-E5530-1.adneomgroup.local>
2017-04-04 14:55:53 +0200

    initializing project

---
+++ src/main/java/com/happystoreClient/SampleUiApplication.java
@@ -0,0 +1,12 @@
+package com.happystoreClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleUiApplication.class, args);
+	}
+}
