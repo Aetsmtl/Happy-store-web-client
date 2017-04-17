3ab6870bac8a8f4a39a0cc09145aa6c612f5b519
firelons <firelons@yahoo.fr>
2017-04-17 22:23:50 +0200

    Voili voilà

--- src/main/java/com/happystoreClient/SampleUiApplication.java
+++ src/main/java/com/happystoreClient/SampleUiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleUiApplication {
-
+// dfjdfjjdffjdhjhfjdjf
 	public static void main(String[] args) {
 		SpringApplication.run(SampleUiApplication.class, args);
 	}
