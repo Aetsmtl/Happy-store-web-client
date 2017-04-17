4e59077c7445f3e572b0ce786405963125d6b591
firelons <firelons@yahoo.fr>
2017-04-17 22:26:05 +0200

    VOICI MON COMMIT

--- src/main/java/com/happystoreClient/SampleUiApplication.java
+++ src/main/java/com/happystoreClient/SampleUiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleUiApplication {
-// dfjdfjjdffjdhjhfjdjf
+// dfjdfjjdffjdhjhfj
 	public static void main(String[] args) {
 		SpringApplication.run(SampleUiApplication.class, args);
 	}
